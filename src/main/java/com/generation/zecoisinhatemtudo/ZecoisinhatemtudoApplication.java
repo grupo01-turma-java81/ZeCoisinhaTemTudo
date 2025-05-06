adc6d36b82276ba448f88f7f53d08432f9023633
ÃIago Wilian <agowillian22@gmail.com>
2025-05-06 10:53:11 -0300

    criacao do projeto

---
+++ src/main/java/com/generation/zecoisinhatemtudo/ZecoisinhatemtudoApplication.java
@@ -0,0 +1,13 @@
+package com.generation.zecoisinhatemtudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZecoisinhatemtudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZecoisinhatemtudoApplication.class, args);
+	}
+
+}
